71a5a26885a69c78e2905de31a6299a805294485
goswamibiswarup369-ops <goswamibiswarup369@gmail.com>
2026-04-27 21:58:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import "./styles/app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
