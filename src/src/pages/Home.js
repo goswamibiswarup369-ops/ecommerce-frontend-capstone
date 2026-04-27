bb1de9ebdbcfd741997dcff84a08205d5b187efb
goswamibiswarup369-ops <goswamibiswarup369@gmail.com>
2026-04-27 22:10:47 +0530

    Add Home component with ProductList integration

---
+++ src/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "../components/ProductList/ProductList";
+
+function Home() {
+  return (
+    <div className="page">
+      <h1>Products</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default Home;
