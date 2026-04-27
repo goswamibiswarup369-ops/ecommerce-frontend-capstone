8c79ad02a437c93eafa6280b120a759530e80336
goswamibiswarup369-ops <goswamibiswarup369@gmail.com>
2026-04-27 22:12:06 +0530

    Create CartPage.js

---
+++ src/src/pages/CartPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cart from "../components/Cart/Cart";
+
+function CartPage() {
+  return (
+    <div className="page">
+      <h1>Your Cart</h1>
+      <Cart />
+    </div>
+  );
+}
+
+export default CartPage;
